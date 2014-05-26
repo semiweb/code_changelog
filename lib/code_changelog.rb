c7c296f198fbe1f4c8fc4499a2d0d9695556dacb
Alexis François <alexis.francois.eu@gmail.com>
2014-05-26 14:11:55 -0400

    Add date functions

--- lib/code_changelog.rb
+++ lib/code_changelog.rb
@@ -1,36 +1,78 @@
 require 'code_changelog/version'
-#require 'generators/migration_generator'
-#require 'generators/code_changelog_generator'
 
 module CodeChangelog
-  class CodeChangelog < ActiveRecord::Base
+  class CodeChangelogEntry < ActiveRecord::Base
   end
 
-  class CodeChangelogManager
-    def send_mail()
-      changelogs = self._all_changelogs()
-      self._exclude_already_done(changelogs)
-      changelogs.each do |cl|
-        changelog = YAML.load_file("#{Rails.root}/doc/changelog/#{cl[:filename]}")
-        puts changelog
-        puts changelog['description']
-        CodeChangelog.new({'version' => cl[:timestamp]}).save
+  class CodeChangelog
+    def initialize(start_date=nil, end_date=nil, show_committed=false)
+      @start_date     = start_date
+      @end_date       = end_date
+      @show_committed = show_committed
+      @changelogs     = self.all_changelogs()
+
+      if not @show_committed
+        self.exclude_already_done(@changelogs)
+      end
+      self.keep_from_date_to_date()
+      @changelogs.each do |f|
+        f[:content] = YAML.load_file("#{Rails.root}/doc/changelog/#{f[:filename]}")
       end
     end
 
-    def _exclude_already_done(changelogs)
-      already_done = CodeChangelog.all.map{|cc| cc[:version]}
+    def generate_content()
+      content = ''
+      @changelogs.each do |cl|
+        content << cl.content['description']
+        content << "\n"
+      end
+      content
+    end
+
+    def generate_list()
+      @changelogs
+    end
+
+    def self.from_date_to_date(start_date, end_date)
+      CodeChangelog.new(start_date, end_date)
+    end
+
+    def self.from_date_to_date_from_uncommitted(start_date, end_date)
+      CodeChangelog.new(start_date, end_date, true)
+    end
+
+    def commit()
+      changelogs = @changelogs.clone
+      self.exclude_already_done(changelogs)
+      changelogs.each do |cl|
+        CodeChangelogEntry.new({'version' => cl[:timestamp]}).save
+      end
+    end
+
+    #private
+
+    def keep_from_date_to_date()
+      return if @start_date.blank? or @end_date.blank?
+
+      @changelogs.select! do |cl|
+        @start_date <= cl[:timestamp] and cl[:timestamp] <= @end_date
+      end
+    end
+
+    def exclude_already_done(changelogs)
+      already_done = Set.new(CodeChangelogEntry.pluck(:version))
       changelogs.reject! do |cl|
         already_done.include? cl[:timestamp]
       end
     end
 
-    def _all_changelogs()
-      all_files = Dir.entries("#{Rails.root}/doc/changelog")
-      all_files.select! do |f|
+
+    def all_changelogs()
+      changelogs = Dir.entries("#{Rails.root}/doc/changelog")
+      changelogs.select! do |f|
         /^\d{14}_.*\.yml$/.match(f)
       end
-      all_files.map do |f|
+      changelogs.map do |f|
         {filename: f,
          timestamp: f[/^\d{14}/, 0]}
       end
--- lib/generators/code_changelog/migration_generator.rb
+++ lib/generators/code_changelog/migration_generator.rb
@@ -2,10 +2,10 @@ module CodeChangelog
   class MigrationGenerator < Rails::Generators::Base
     desc "This generator creates the migration file"
     def create_migration_file
-      create_file "db/migrate/#{Time.now.utc.strftime("%Y%m%d%H%M%S")}_code_changelog_migration.rb", <<DATA
-class CodeChangelogMigration < ActiveRecord::Migration
+      create_file "db/migrate/#{Time.now.utc.strftime("%Y%m%d%H%M%S")}_code_changelog_entry_migration.rb", <<DATA
+class CodeChangelogEntryMigration < ActiveRecord::Migration
   def change
-    create_table :code_changelogs do |t|
+    create_table :code_changelog_entries do |t|
       t.string :version
     end
   end
